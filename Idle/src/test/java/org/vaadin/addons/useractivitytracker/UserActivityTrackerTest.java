3361fd3d06b66441074c3651ef61f7c7a9ab033b
Sami Ekblad <sami@vaadin.com>
2014-07-15 22:44:08 +0300

    Initial commit

---
+++ Idle/src/test/java/org/vaadin/addons/useractivitytracker/UserActivityTrackerTest.java
@@ -0,0 +1,13 @@
+package org.vaadin.addons.useractivitytracker;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class UserActivityTrackerTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
