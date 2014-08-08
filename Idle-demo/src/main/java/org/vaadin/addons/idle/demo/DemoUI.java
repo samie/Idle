857ca3707118b8aa75bfb4fc2f982e885471e123
Max Schuster <msc@schmelzermedien.de>
2014-08-08 11:00:53 +0200

    Annotate UI with @PreserveOnRefresh to test the add-ons behaviour when the page gets refreshed and the UI is preserved.

--- Idle-demo/src/main/java/org/vaadin/addons/idle/demo/DemoUI.java
+++ Idle-demo/src/main/java/org/vaadin/addons/idle/demo/DemoUI.java
@@ -1,5 +1,6 @@
 package org.vaadin.addons.idle.demo;
 
+import com.vaadin.annotations.PreserveOnRefresh;
 import org.vaadin.addons.idle.Idle;
 
 import javax.servlet.annotation.WebServlet;
@@ -7,15 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("demo")
 @Title("Idle Add-on Demo")
+@PreserveOnRefresh
 @SuppressWarnings("serial")
 public class DemoUI extends UI {
 
@@ -28,6 +32,8 @@ public class DemoUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
+        
+        VerticalLayout wrapperLayout = new VerticalLayout();
 
         // Simple status label
         status = new Label("You are now active");
@@ -46,12 +52,27 @@ public class DemoUI extends UI {
                 status.setValue("You are now active");
             }
         });
+        
+        wrapperLayout.addComponent(status);
+        wrapperLayout.setComponentAlignment(status, Alignment.MIDDLE_CENTER);
+        
+        Button restartButton = new Button("Restart Application");
+        restartButton.addClickListener(new Button.ClickListener() {
 
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                Page.getCurrent().setLocation("?restartApplication");
+            }
+        });
+        wrapperLayout.addComponent(restartButton);
+        wrapperLayout.setComponentAlignment(
+                restartButton, Alignment.MIDDLE_CENTER);
+        
         // Show it in the middle of the screen
         final VerticalLayout layout = new VerticalLayout();
         layout.setSizeFull();
-        layout.addComponent(status);
-        layout.setComponentAlignment(status, Alignment.MIDDLE_CENTER);
+        layout.addComponent(wrapperLayout);
+        layout.setComponentAlignment(wrapperLayout, Alignment.MIDDLE_CENTER);
         setContent(layout);
 
     }
